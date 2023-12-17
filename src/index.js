b9f0a4efd758d06357fc22162992067b88a02bc3
Shri Ram <shriram.k.raja@gmail.com>
2023-12-16 20:21:47 -0500

    scroll going to top bug fixed on navigation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppWideProvider from './providers/appWide/provider';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppWideProvider>
+      <ScrollToTop />
       <App />
     </AppWideProvider>
   </React.StrictMode>
